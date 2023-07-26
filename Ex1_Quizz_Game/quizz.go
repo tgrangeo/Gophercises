9aab562daf2d0b96ca927195d4f96936938c8013
tgrangeo <thomas.grangeon9@gmail.com>
2023-07-26 16:44:25 +0200

    ex1

--- Ex1_Quizz_Game/quizz.go
+++ Ex1_Quizz_Game/quizz.go
@@ -3,16 +3,28 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type ProblemItem struct {
 	Question string
 	Answer   int
+}
+
+func shuffle(items []ProblemItem) {
+	rand.Seed(time.Now().UnixNano())
+	n := len(items)
+	for i := n - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		items[i], items[j] = items[j], items[i]
+	}
 }
 
 func main() {
@@ -40,13 +52,40 @@ func main() {
 		})
 	}
 
+	//read
+	reader := bufio.NewReader(os.Stdin)
+
+	// Define flags
+	var flagVar int
+	flag.IntVar(&flagVar, "time", 30, "Duration for the timer")
+
+	var shuffleFlag bool
+	flag.BoolVar(&shuffleFlag, "shuffle", false, "Set to true to shuffle questions")
+	flag.Parse()
+	if shuffleFlag {
+		shuffle(ProblemList)
+	}
+
+	//timer begin
+	fmt.Print("Do you want to begin the quiz ? y/n\n")
+	text, _ := reader.ReadString('\n')
+	if strings.TrimSpace(text) != "y" {
+		fmt.Println("See you next time :)")
+		return
+	}
+	var timer time.Duration = time.Duration(flagVar)
+	go func() {
+		<-time.After(timer * time.Second)
+		fmt.Println(flagVar, "seconds have passed. Program will now exit.")
+		os.Exit(1)
+	}()
+
 	//game begin
 	var correct_answer int
-	reader := bufio.NewReader(os.Stdin)
 	for _, item := range ProblemList {
 		fmt.Println(item.Question)
 		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text) //why atoi in go is different from atoi in c ????
+		text = strings.TrimSpace(text)
 		userOutput, _ := strconv.Atoi(text)
 		if userOutput == item.Answer {
 			correct_answer++
---
+++ readme.md
@@ -0,0 +1,2 @@
+exercises in go 
+thx Gophercises
